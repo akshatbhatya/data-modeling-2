995d6749bbae85d3324fbdeef18843d7f94f34a8
akshat <akshatbhatya@gmail.com>
2024-03-28 13:46:07 +0530

    hospital data is done

--- models/Hospital/hospital.model.js
+++ models/Hospital/hospital.model.js
@@ -5,7 +5,28 @@ const hospitalSchema=new mongoose.Schema({
     hospitalName:{
         type:String,
         required:true
-
+    },
+    specializedIn:[
+        {
+            type:String,
+            required:true
+        }
+    ],
+    address1:{
+        type:String,
+        required:true
+    },
+    address2:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    pincode:{
+        type:String,
+        required:true
     }
 
 },{timestamps:true})
